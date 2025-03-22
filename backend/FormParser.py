a98d49a288ec4d06d581b9990f0ae398f4f6a5d6
AdKamat2411 <adkamat@uwaterloo.ca>
2025-03-22 16:33:11 -0400

    kvp for form parser

--- backend/FormParser.py
+++ backend/FormParser.py
@@ -1,64 +1,131 @@
-from typing import Optional
+
+from typing import Optional, Sequence
 
 from google.api_core.client_options import ClientOptions
-from google.cloud import documentai  # type: ignore
+from google.cloud import documentai
 
 # TODO(developer): Uncomment these variables before running the sample.
 # project_id = "YOUR_PROJECT_ID"
 # location = "YOUR_PROCESSOR_LOCATION" # Format is "us" or "eu"
 # processor_id = "YOUR_PROCESSOR_ID" # Create processor before running sample
+# processor_version = "rc" # Refer to https://cloud.google.com/document-ai/docs/manage-processor-versions for more information
 # file_path = "/path/to/local/pdf"
 # mime_type = "application/pdf" # Refer to https://cloud.google.com/document-ai/docs/file-types for supported file types
-# field_mask = "text,entities,pages.pageNumber"  # Optional. The fields to return in the Document object.
-# processor_version_id = "YOUR_PROCESSOR_VERSION_ID" # Optional. Processor version to use
 
-
-def process_document_sample(
+def process_document_form_sample(
     project_id: str,
     location: str,
     processor_id: str,
+    processor_version: str,
     file_path: str,
     mime_type: str,
-    field_mask: Optional[str] = None,
-    processor_version_id: Optional[str] = None,
+) -> documentai.Document:
+    # Online processing request to Document AI
+    document = process_document(
+        project_id, location, processor_id, processor_version, file_path, mime_type
+    )
+
+    # Read the table and form fields output from the processor
+    # The form processor also contains OCR data. For more information
+    # on how to parse OCR data please see the OCR sample.
+
+    text = document.text
+    print(f"Full document text: {repr(text)}\n")
+    print(f"There are {len(document.pages)} page(s) in this document.")
+
+    # Read the form fields and tables output from the processor
+    for page in document.pages:
+        print(f"\n\n**** Page {page.page_number} ****")
+
+        print(f"\nFound {len(page.tables)} table(s):")
+        for table in page.tables:
+            num_columns = len(table.header_rows[0].cells)
+            num_rows = len(table.body_rows)
+            print(f"Table with {num_columns} columns and {num_rows} rows:")
+
+            # Print header rows
+            print("Columns:")
+            print_table_rows(table.header_rows, text)
+            # Print body rows
+            print("Table body data:")
+            print_table_rows(table.body_rows, text)
+
+        print(f"\nFound {len(page.form_fields)} form field(s):")
+        for field in page.form_fields:
+            name = layout_to_text(field.field_name, text)
+            value = layout_to_text(field.field_value, text)
+            print(f"    * {repr(name.strip())}: {repr(value.strip())}")
+
+    # Supported in version `pretrained-form-parser-v2.0-2022-11-10` and later.
+    # For more information: https://cloud.google.com/document-ai/docs/form-parser
+    if document.entities:
+        print(f"Found {len(document.entities)} generic entities:")
+        for entity in document.entities:
+            print_entity(entity)
+            # Print Nested Entities
+            for prop in entity.properties:
+                print_entity(prop)
+
+    return document
+
+def print_table_rows(
+    table_rows: Sequence[documentai.Document.Page.Table.TableRow], text: str
 ) -> None:
+    for table_row in table_rows:
+        row_text = ""
+        for cell in table_row.cells:
+            cell_text = layout_to_text(cell.layout, text)
+            row_text += f"{repr(cell_text.strip())} | "
+        print(row_text)
+
+def print_entity(entity: documentai.Document.Entity) -> None:
+    # Fields detected. For a full list of fields for each processor see
+    # the processor documentation:
+    # https://cloud.google.com/document-ai/docs/processors-list
+    key = entity.type_
+
+    # Some other value formats in addition to text are available
+    # e.g. dates: `entity.normalized_value.date_value.year`
+    text_value = entity.text_anchor.content or entity.mention_text
+    confidence = entity.confidence
+    normalized_value = entity.normalized_value.text
+    print(f"    * {repr(key)}: {repr(text_value)} ({confidence:.1%} confident)")
+
+    if normalized_value:
+        print(f"    * Normalized Value: {repr(normalized_value)}")
+
+def process_document(
+    project_id: str,
+    location: str,
+    processor_id: str,
+    processor_version: str,
+    file_path: str,
+    mime_type: str,
+    process_options: Optional[documentai.ProcessOptions] = None,
+) -> documentai.Document:
     # You must set the `api_endpoint` if you use a location other than "us".
-    opts = ClientOptions(api_endpoint=f"{location}-documentai.googleapis.com")
-
-    client = documentai.DocumentProcessorServiceClient(client_options=opts)
-
-    if processor_version_id:
-        # The full resource name of the processor version, e.g.:
-        # `projects/{project_id}/locations/{location}/processors/{processor_id}/processorVersions/{processor_version_id}`
-        name = client.processor_version_path(
-            project_id, location, processor_id, processor_version_id
+    client = documentai.DocumentProcessorServiceClient(
+        client_options=ClientOptions(
+            api_endpoint=f"{location}-documentai.googleapis.com"
         )
-    else:
-        # The full resource name of the processor, e.g.:
-        # `projects/{project_id}/locations/{location}/processors/{processor_id}`
-        name = client.processor_path(project_id, location, processor_id)
+    )
+
+    # The full resource name of the processor version, e.g.:
+    # `projects/{project_id}/locations/{location}/processors/{processor_id}/processorVersions/{processor_version_id}`
+    # You must create a processor before running this sample.
+    name = client.processor_version_path(
+        project_id, location, processor_id, processor_version
+    )
 
     # Read the file into memory
     with open(file_path, "rb") as image:
         image_content = image.read()
 
-    # Load binary data
-    raw_document = documentai.RawDocument(content=image_content, mime_type=mime_type)
-
-    # For more information: https://cloud.google.com/document-ai/docs/reference/rest/v1/ProcessOptions
-    # Optional: Additional configurations for processing.
-    process_options = documentai.ProcessOptions(
-        # Process only specific pages
-        individual_page_selector=documentai.ProcessOptions.IndividualPageSelector(
-            pages=[1]
-        )
-    )
-
     # Configure the process request
     request = documentai.ProcessRequest(
         name=name,
-        raw_document=raw_document,
-        field_mask=field_mask,
+        raw_document=documentai.RawDocument(content=image_content, mime_type=mime_type),
+        # Only supported for Document OCR processor
         process_options=process_options,
     )
 
@@ -66,26 +133,27 @@ def process_document_sample(
 
     # For a full list of `Document` object attributes, reference this page:
     # https://cloud.google.com/document-ai/docs/reference/rest/v1/Document
-    document = result.document
+    return result.document
 
-    # Read the text recognition output from the processor
-    # print("The document is classified as: ")
-    print(document)
-    # maxVal = float('-inf')
-    # maxKey = ""
-    # for entity in document.entities:
-    #     if entity.confidence > maxVal:
-    #         maxVal = entity.confidence
-    #         maxKey = entity.type_
-    # print(f"Document classified as: {maxKey}")
+def layout_to_text(layout: documentai.Document.Page.Layout, text: str) -> str:
+    """
+    Document AI identifies text in different parts of the document by their
+    offsets in the entirety of the document"s text. This function converts
+    offsets to a string.
+    """
+    # If a text segment spans several lines, it will
+    # be stored in different text segments.
+    return "".join(
+        text[int(segment.start_index) : int(segment.end_index)]
+        for segment in layout.text_anchor.text_segments
+    )
 
 # Example usage:
-process_document_sample(
+process_document_form_sample(
     project_id="genesis-genai-454505",
     location="us",
     processor_id="9c40568fce3ffba0",
-    file_path="sample1.pdf",
+    file_path="form1.pdf",
     mime_type="application/pdf",
-    field_mask="text,entities,pages.pageNumber",
-    processor_version_id="pretrained-form-parser-v2.1-2023-06-26"
+    processor_version="pretrained-form-parser-v2.1-2023-06-26"
 )
